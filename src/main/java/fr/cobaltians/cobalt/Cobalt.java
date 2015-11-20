87929238ebe47e05c029382d496a007bb34b606a
Myrdhinn <sebastien.famel@gmail.com>
2015-11-20 17:51:28 +0100

    add getter path from asset

--- src/main/java/fr/cobaltians/cobalt/Cobalt.java
+++ src/main/java/fr/cobaltians/cobalt/Cobalt.java
@@ -261,6 +261,8 @@ public class Cobalt {
         else mResourcePath = "";
 	}
 
+    public String getResourcePathFromAsset() {return mResourcePath;}
+
     public static Context getAppContext() {
         return mContext;
     }
