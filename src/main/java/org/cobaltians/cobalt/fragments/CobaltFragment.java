00b6f1fa8a7a149a3539843b2d3e2e0b8b53c26f
Myrdhinn <sebastien.famel@gmail.com>
2016-06-29 10:46:14 +0200

    cobaltians/Cobalt#123 : fix bug refresh Webview on android 4.2.2

--- src/main/java/org/cobaltians/cobalt/fragments/CobaltFragment.java
+++ src/main/java/org/cobaltians/cobalt/fragments/CobaltFragment.java
@@ -323,6 +323,11 @@ public abstract class CobaltFragment extends Fragment implements IScrollListener
             }
         };
 
+        // Fix bug #123 on android 4.2.2 for render refresh view
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
         mWebView.setWebViewClient(webViewClient);
     }
 
